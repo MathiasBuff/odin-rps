3161e8cb4a50dd2a31237a32ab053473579fd1fe
MathiasBuff <92915426+MathiasBuff@users.noreply.github.com>
2023-05-08 20:10:11 +0200

    Add basic UI and logic

--- js/rps.js
+++ js/rps.js
@@ -1,9 +1,54 @@
+let computerScore = 0;
+let playerScore = 0;
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+for (key in choices) {
+    let button = document.querySelector("body").appendChild(document.createElement("button"));
+    button.setAttribute("class", "playerSelection");
+    button.setAttribute("value", `${choices[key].toLowerCase()}`);
+    button.textContent = choices[key];
+}
+
+let roundResultDisplay = document.querySelector("body").appendChild(document.createElement("div"));
+
+let scoreDisplay = document.querySelector("body").appendChild(document.createElement("div"));
+updateScoreDisplay();
+
+let matchResultDisplay = document.querySelector("body").appendChild(document.createElement("div"));
+
+
+const buttons = document.querySelectorAll(".playerSelection");
+buttons.forEach((button) => {
+    console.log(button.value);
+    button.addEventListener("click", (event) => {playRound(event.target.value, getComputerChoice());});
+})
+
+function updateScoreDisplay() {
+    scoreDisplay.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+    checkVictory();
+}
+
 function getComputerChoice() {
     return ["rock", "paper", "scissors"][Math.floor(Math.random()*3)]
 }
 
-function getPlayerChoice() {
-    return prompt("What move would you like to play?")
+function checkVictory() {
+    switch (true) {
+        case (playerScore < 5 && computerScore < 5): break;
+        case (playerScore == 5 && computerScore < 5): {
+            matchResultDisplay.textContent = "You Win the Game! Congratulations!";
+            break;
+        }
+        case (computerScore == 5 && playerScore < 5): {
+            matchResultDisplay.textContent = "You Lose the Game... Try again?";
+            break;
+        }
+        default: {
+            matchResultDisplay.textContent = "Oi! You're supposed to reset and play another game!";
+            break;
+        }
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -11,7 +56,7 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (possibleValues.includes(playerSelection) == false) {
-        console.log(`Invalid value: ${playerSelection}. Round skipped.`)
+        roundResultDisplay.textContent = `Invalid value: ${playerSelection}. Round skipped.`;
         return null
     }
 
@@ -19,52 +64,54 @@ function playRound(playerSelection, computerSelection) {
 
     switch (computerSelection) {
         case possibleValues.at((possibleValues.indexOf(playerSelection) + 1) % 3): {
-            message = `You Win! ${playerSelection} beats ${computerSelection}!`
-            score = 1
+            message = `You Win! ${playerSelection} beats ${computerSelection}!`;
+            playerScore += 1;
             break;
         }
         case playerSelection: {
-            message = `It's a Tie! Both players chose ${computerSelection}!`
-            score = 0
+            message = `It's a Tie! Both players chose ${computerSelection}!`;
             break;
         }
         case possibleValues.at((possibleValues.indexOf(playerSelection) - 1) % 3): {
-            message = `You Lose! ${computerSelection} beats ${playerSelection}!`
-            score = -1
+            message = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+            computerScore += 1;
             break;
         }
         default: {
             message = "Unexpected error."
-            score = null
+            score = null;
         }
     }
-    console.log(message);
-    return score
+    roundResultDisplay.textContent =  message;
+    updateScoreDisplay();
+    return score;
 }
 
-function game(numberOfRounds) {
-    let message;
-    let totalScore = 0;
+// function getPlayerChoice() {
+//     return prompt("What move would you like to play?")
+// }
 
-    for (let i = 0; i < numberOfRounds; i++) {
-        totalScore += playRound(getPlayerChoice(), getComputerChoice());
-    }
-    console.log(totalScore);
-    switch (true) {
-        case totalScore > 0: {
-            message = "You Won the Game! Well Done!"
-            break;
-        }
-        case totalScore == 0: {
-            message = "It's a Tie! What a Riveting Match!"
-            break;
-        }
-        case totalScore < 0: {
-            message = "You Lost the Game, Try Again!"
-            break;
-        }
-    }
-    console.log(message);
-}
+// function game(numberOfRounds) {
+//     let message;
+//     let totalScore = 0;
 
-game(3)
+//     for (let i = 0; i < numberOfRounds; i++) {
+//         totalScore += playRound(getPlayerChoice(), getComputerChoice());
+//     }
+//     console.log(totalScore);
+//     switch (true) {
+//         case totalScore > 0: {
+//             message = "You Won the Game! Well Done!"
+//             break;
+//         }
+//         case totalScore == 0: {
+//             message = "It's a Tie! What a Riveting Match!"
+//             break;
+//         }
+//         case totalScore < 0: {
+//             message = "You Lost the Game, Try Again!"
+//             break;
+//         }
+//     }
+//     console.log(message);
+// }
